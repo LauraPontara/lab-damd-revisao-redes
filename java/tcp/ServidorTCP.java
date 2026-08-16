6c313cdaa4ddc5feff23886f2cbad2e11dd2cb09
LauraPontara <lauracplopes@gmail.com>
2026-08-16 16:37:40 -0300

    feat(tcp): implementa servidor e cliente TCP em Java

---
+++ java/tcp/ServidorTCP.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+import java.time.LocalTime;
+
+public class ServidorTCP {
+    public static void main(String[] args) throws IOException {
+        int porta = 5000;
+        try (ServerSocket servidor = new ServerSocket(porta)) {
+            System.out.println("[TCP] Servidor aguardando conexões na porta " + porta + "...");
+            try (Socket cliente = servidor.accept();
+                    BufferedReader entrada = new BufferedReader(
+                            new InputStreamReader(cliente.getInputStream()));
+                    PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true)) {
+
+                System.out.println("[TCP] Cliente conectado: " + cliente.getRemoteSocketAddress());
+                String mensagem;
+                while ((mensagem = entrada.readLine()) != null) {
+                    System.out.println("[TCP] Recebido: " + mensagem);
+                    if (mensagem.equalsIgnoreCase("sair")) {
+                        saida.println("Encerrando conexão. Até mais!");
+                        break;
+                    }
+
+                    if (mensagem.equalsIgnoreCase("hora")) {
+                        saida.println("Monitor responde: agora são " + LocalTime.now());
+                    } else {
+                        saida.println("Monitor responde: recebi sua mensagem -> \"" + mensagem + "\"");
+                    }
+                }
+            }
+        }
+        System.out.println("[TCP] Servidor encerrado.");
+    }
+}
